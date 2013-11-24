c3ab683a80bb2a1fb63227b91be3ae998b1599cb
Christian Sigg <christian@sigg-iten.ch>
2013-11-24 15:11:05 +0100

    added tryonly option

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 tarpar
 ======
 
-A barebones Python script for backup and archival of a Mac internal HD using tar and par2
+A barebones Python script for backup and archival of a Mac internal harddisk using tar and par2, see my [backup strategy] (http://sigg-iten.ch/learningbits/2013/11/22/my-strategy-for-personal-backups/) for more details.
--- tarpar.py
+++ tarpar.py
@@ -5,10 +5,12 @@ from datetime import date
 import subprocess
 import socket
 
-parser = argparse.ArgumentParser(description='Backup internal disk.')
-parser.add_argument('destination', help='backup destination path')
+parser = argparse.ArgumentParser(description='Create a tar and par2 archive of selected folders of the internal disk. The name of the archive is derived from the hostname and current date.')
+parser.add_argument('--tryonly', action='store_true', help='print but do not execute backup commands')
+parser.add_argument('destination', help='path to external backup destination, e.g. /Volumes/bup1')
 args = parser.parse_args()
 
+# folders to backup
 sources = [
     "/Applications", 
     "/Library",
@@ -22,21 +24,18 @@ today = date.today().isoformat()
 hostname = socket.gethostname().split('.')[0]
 destination = hostname + "_" + today
 
-### 
-# create backup using tar
+bupcmd = "gtar -cvW -f " + args.destination + "/" + destination + ".tar " + " ".join(sources) + " 2> " + args.destination +  "/" + destination + ".err"
 
-bupcmd = "caffeinate -s gtar -cvW -f " + args.destination + "/" + destination + ".tar " + " ".join(sources) + " 2> " + args.destination +  "/" + destination + ".err"
+parcreatecmd = "par2 c -n1 -r5 -vv " + args.destination + "/" + destination + ".tar"
+parverifycmd = "par2 v -vv " + args.destination + "/" + destination + ".tar"
 
-print(bupcmd)
-subprocess.call(bupcmd, shell = True)
-
-###
-# create parchive
-
-parcmd = "caffeinate -s par2 c -n1 -r5 -vv " + args.destination + "/" + destination + ".tar && par2 v -vv " + args.destination + "/" + destination + ".tar"
-
-print(parcmd)
-subprocess.call(parcmd, shell = True)
+if (args.tryonly):
+    print(bupcmd)
+    print(parcreatecmd)
+    print(parverifycmd)
+else:
+    cmd = bupcmd + " && " + parcreatecmd + " && " + parverifycmd
+    subprocess.call(cmd, shell = True)
 
 
 
